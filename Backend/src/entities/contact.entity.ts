38a705567fc7ddb7149f265dda574c1450424548
Fernando <fernandogynbsb@gmail.com>
2023-02-02 11:43:45 -0300

    criando esqueleto da entity de contatos

---
+++ Backend/src/entities/contact.entity.ts
@@ -0,0 +1,31 @@
+import {
+    Entity,
+    Column,
+    PrimaryGeneratedColumn,
+    CreateDateColumn,
+    UpdateDateColumn,
+    ManyToOne,
+    JoinColumn,
+} from "typeorm";
+
+@Entity("contacts")
+export class Contact {
+    @PrimaryGeneratedColumn("uuid")
+    readonly id: string;
+
+    @Column({ length: 60 })
+    name: string;
+
+    @Column({ length: 60, unique: true })
+    email: string;
+
+    @Column({ length: 14 })
+    telephone: string;
+
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @UpdateDateColumn()
+    updatedAt: Date;
+
+}
